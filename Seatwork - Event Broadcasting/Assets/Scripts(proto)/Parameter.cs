1eff5c036e125a27b9ae2f76802d29a765eaff82
Derek-Colbee-Uy <derek_colbee_uy@dlsu.edu.ph>
2024-05-23 13:30:33 +0800

    Added the package

---
+++ Seatwork - Event Broadcasting/Assets/Scripts(proto)/Parameter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parameter : MonoBehaviour
+{
+    public void GetIntExtra(int max, int min)
+    {
+
+    }
+}
